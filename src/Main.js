449bb9f9aff63f6fe3d874f13f8a2a8881c384fa
Ahsan <ahsan@technovilla.net>
2021-08-13 19:29:59 +0600

    Initial setup

---
+++ src/Main.js
@@ -0,0 +1,13 @@
+import './Main.css';
+
+function Main() {
+  return (
+    <div className="main">
+      <header className="header">
+        
+      </header>
+    </div>
+  );
+}
+
+export default Main;
