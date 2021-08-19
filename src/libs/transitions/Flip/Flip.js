7de62b71f7014af07c040b06b29856d6f4774cf3
Ahsan <ahsan@technovilla.net>
2021-08-19 14:17:51 +0600

    Flip transition improvement

--- src/libs/transitions/Flip/Flip.js
+++ src/libs/transitions/Flip/Flip.js
@@ -1,5 +1,5 @@
-import React from "react";
-import './Flip.css';
+import React, { useState } from "react";
+import './Style.css';
 
 const FlipFront = (props) => {
   return (
@@ -20,18 +20,36 @@ const FlipBack = (props) => {
 const Flip = (props)  => {
   const style = {
     width: props.width,
-    height: props.height,
-    perspective: props.swing ? '1000px' : '0'
+    height: props.height
   };
+
+  const initialClassName = `flip ${props.variant} ${props.event}${props.vertical ? ' vertical': ' horizontal'}`;
+
+  const [classState, setClass] = useState(initialClassName);
+  const [isClicked, setClicked] = useState(false);
+
+  const onClick = () => {
+    const clicked = !isClicked;
+    setClicked(clicked);
+    if (props.event === "click") {
+      setClass(clicked ? (initialClassName + " clicked") : initialClassName);
+    }
+  }
   
   return (
-    <div className='flip' style={style}>
-      <div className='flip-inner'>
+    <div className={classState} style={style} onClick={onClick}>
+      <div className="flip-inner">
         {props.children}
       </div>
     </div>
   );
 }
+
+Flip.defaultProps = {
+  variant: 'swing', // possible values are 'flat', 'swing', 'slide'
+  vertical: false,
+  event: 'hover' // possible values are 'click', 'hover'
+};
 
 export default Object.assign(Flip, {
   Front: FlipFront,
